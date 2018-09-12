6ce3a606bc785bb085427859af8c3d93e6874496
Claus Ibsen <claus.ibsen@gmail.com>
2018-09-12 13:05:31 +0200

    Client with just Camel retry

--- boot/src/main/java/com/foo/MyRoute.java
+++ boot/src/main/java/com/foo/MyRoute.java
@@ -1,38 +1,25 @@
 package com.foo;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
- * Uses the Hystrix Circuit Breaker.
- *
- * See also the <tt>application.properties</tt> file where
- * Hystrix has been configured, and the <tt>src/main/fabric8/service.yml</tt> file
- * which adds a label to instruct the Turbine/Hystrix Dashboard that this
- * container runs Hystrix.
+ * Uses Camel Retry.
  */
 @Component
 public class MyRoute extends RouteBuilder {
 
-    // the configuration value (fallback)
-    // has been defined in the <tt>src/main/fabric8/deployment.yml</tt> file
-
-    // inject configuration via spring-style @Value
-    @Value("${fallback}")
-    private String fallback;
-
     @Override
     public void configure() throws Exception {
+        // use Camel retry
+        // to retry up till 10 times with 1 second delay
+//        onException(Exception.class)
+//            .redeliveryDelay(1000)
+//            .maximumRedeliveries(10);
+
+
         from("timer:foo?period=1000")
-            .hystrix().id("call-helloswarm")
-                // use Camel {{service}} to lookup service (pluggable service providers)
-                //.to("http4:{{service:helloswarm}}/hello?connectionClose=true")
-                // just use DNS name to lookup service in k8s
-                .to("http4:helloswarm:8080/hello?connectionClose=true")
-            .onFallback()
-                .setBody().simple("{{fallback}}") // inject via Camel simple language
-            .end()
+            .to("http4:helloswarm:8080/hello?connectionClose=true")
             .log("${body}");
     }
 }
